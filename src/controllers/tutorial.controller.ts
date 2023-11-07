3f653bbd70ba294aabf56a1f27f0e51773a02040
AbhiramKrishnaM <abhiramkrishna.8921@gmail.com>
2023-11-07 20:30:49 +0530

    added controllers and added response

--- src/controllers/tutorial.controller.ts
+++ src/controllers/tutorial.controller.ts
@@ -1,13 +1,68 @@
 import { Request, Response } from "express";
 
 export default class TutorialController {
-  async create(req: Request, res: Response) {}
+  async create(req: Request, res: Response) {
+    try {
+      res.status(201).json({
+        message: "Create Ok",
+        reqBody: req.body,
+      });
+    } catch (err) {
+      res.status(500).json({
+        message: "Internal Server Error!",
+      });
+    }
+  }
 
-  async findAll(req: Request, res: Response) {}
+  async findAll(req: Request, res: Response) {
+    try {
+      res.status(200).json({
+        message: "Find All Ok",
+      });
+    } catch (err) {
+      res.status(500).json({
+        message: "Internal Server Error!",
+      });
+    }
+  }
 
-  async findOne(req: Request, res: Response) {}
+  async findOne(req: Request, res: Response) {
+    try {
+      res.status(200).json({
+        message: "Find One Ok",
+        reqParamId: req.params.id,
+      });
+    } catch (err) {
+      res.status(500).json({
+        message: "Internal Server Error!",
+      });
+    }
+  }
 
-  async update(req: Request, res: Response) {}
+  async update(req: Request, res: Response) {
+    try {
+      res.status(200).json({
+        message: "Find One Ok",
+        reqParamId: req.params.id,
+        reqBody: req.body,
+      });
+    } catch (err) {
+      res.status(500).json({
+        message: "Internal Server Error!",
+      });
+    }
+  }
 
-  async delete(req: Request, res: Response) {}
+  async delete(req: Request, res: Response) {
+    try {
+      res.status(200).json({
+        message: "Find One Ok",
+        reqParamId: req.params.id,
+      });
+    } catch (err) {
+      res.status(500).json({
+        message: "Internal Server Error!",
+      });
+    }
+  }
 }
